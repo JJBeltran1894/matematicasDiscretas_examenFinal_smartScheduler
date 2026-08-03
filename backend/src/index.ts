b5b962138c16749998cd84484d0bf40c40fc55da
JJBeltran1894 <juanjbeltran94@hotmail.com>
2026-08-03 18:07:55 -0500

    JJBA: Correccion de errores e instalacion de dotenv

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,6 +1,7 @@
 import express from "express";
 import cors from "cors";
 import "./database/prisma.ts";
+import courseRouter from "./routes/course.routes.ts";
 
 const app = express();
 const PORT = 3001;
@@ -8,6 +9,8 @@ const PORT = 3001;
 app.use(cors());
 
 app.use(express.json());
+
+app.use("/api/courses", courseRouter);
 
 app.listen(PORT, () => {
   console.log("SERVIDOR LEVANTADO EN EL PUERTO ", PORT);
