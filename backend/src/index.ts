7c5383b6bb0ae901574304c828630437923c03d9
JJBeltran1894 <juanjbeltran94@hotmail.com>
2026-08-03 23:23:07 -0500

    JJBA: Actualizado index.ts

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -2,6 +2,7 @@ import express from "express";
 import cors from "cors";
 import "./database/prisma.ts";
 import courseRouter from "./routes/course.routes.ts";
+import scheduleRouter from "./routes/schedule.routes.ts";
 
 const app = express();
 const PORT = 3001;
@@ -11,6 +12,7 @@ app.use(cors());
 app.use(express.json());
 
 app.use("/api/courses", courseRouter);
+app.use("/api/schedules", scheduleRouter);
 
 app.listen(PORT, () => {
   console.log("SERVIDOR LEVANTADO EN EL PUERTO ", PORT);
